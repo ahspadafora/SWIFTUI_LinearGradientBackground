33300e74a922c6df349469c4f1defbfd7e35822b
Amber Spadafora <ahelenespadafora@gmail.com>
2022-12-24 20:44:36 -0500

    create example of a linear gradient background in SwiftUI using custom color assets

---
+++ iTrivia/Color+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Ext.swift
+//  iTrivia
+//
+//  Created by Amber Spadafora on 12/24/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let trivYellowGreen = Color("trivYellowGreen")
+    
+    static let tivLightGreen = Color("trivLightGreen")
+}
